e6602d172ca30dbcec8b31c1f4e683397e3738d6
Sarah Grant <sgrant@barbariangroup.com>
2013-01-16 12:52:29 -0500

    added redirect rule so url always displays as hotprobs.com

--- app/config.js
+++ app/config.js
@@ -1,17 +1,16 @@
 module.exports = function(app, exp) {
 
-	// Redirect to xxx.subnod.es
-	/*app.configure('development', function(){ //production
+	// rewrite URL to hotprobs.com
+	app.configure('production', function(){
 		app.use(function(req, res, next){
 			var hostname = req.header("host").split(":")[0];
-			if (hostname != "subnod.es") {
-				res.redirect('http://subnod.es');
+			if (hostname != "www.hotprobs.com") {
+				res.redirect('http://www.hotprobs.com');
 				return;
 			}
-
 			next();
 		});
-	});*/
+	});
 
 	app.configure(function(){
 		app.set('views', app.root + '/app/server/views');
