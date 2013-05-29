c40b8bd8d2cdcb1446bfb137710aa75562c72975
Sarah Grant <sgrant@barbariangroup.com>
2013-05-28 23:11:01 -0400

    adding install scripts; merged lucky into master

---
+++ scripts/configure.sh
@@ -0,0 +1,82 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# SOFTWARE INSTALL
+#
+# update the packages (may take a long time)
+sudo apt-get update && sudo apt-get -y upgrade
+
+# install prerequisite software
+sudo apt-get install -y batctl bridge-utils iw hostapd dnsmasq git-core
+
+# install node.js binary and put it in /usr/local
+wget http://nodejs.org/dist/v0.9.9/node-v0.9.9-linux-arm-pi.tar.gz
+cd /usr/local
+sudo tar xzvf ~/node-v0.9.9-linux-arm-pi.tar.gz --strip=1
+
+# create directory where web apps will live
+cd ~
+mkdir www
+# install subnodes chat room
+#cd www
+#git clone https://github.com/chootka/subnodes.git
+#cd subnodes
+#sudo npm install
+#sudo npm install -g nodemon
+
+# create startup script; starts node app and hostapd on boot
+#sudo cp scripts/subnodes.sh /etc/init.d/subnodes
+#sudo chmod 755 /etc/init.d/subnodes
+#sudo update-rc.d /etc/init.d/subnodes defaults
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# NETWORK CONFIGURATION
+#
+# modify the network interface config file
+sudo python configure_network_interfaces.py
+
+# copy in our hostapd configuration file
+sudo cp conf/hostapd.conf /etc/hostapd/hostapd.conf
+
+# copy in our hostapd init script
+sudo cp conf/hostapd /etc/default/hostapd
+
+# copy in our dnsmasq configuration file
+sudo cp conf/dnsmasq.conf /etc/dnsmasq.conf
+
+# delete default interfaces
+sudo ifconfig wlan0 down
+sudo iw dev wlan0 del
+sudo ifconfig wlan1 down
+sudo iw dev wlan1 del
+
+# set up batman (mesh networking)
+sudo modprobe batman-adv
+sudo iw phy phy0 interface add mesh0 type adhoc
+sudo ifconfig mesh0 mtu 1528
+sudo iwconfig mesh0 mode ad-hoc essid meshnet ap 02:12:34:56:78:90 channel 3
+sudo ifconfig mesh0 down
+sudo batctl if add mesh0
+sudo ifconfig mesh0 up
+
+#set up AP
+batctl ap_isolation 1
+sudo iw phy phy1 interface add ap0 type __ap
+
+#set up bridge
+sudo brctl addbr br0
+sudo brctl addif br0 ap0
+sudo brctl addif br0 bat0
+sudo ifconfig bat0 up
+
+# give the RPi a static IP address for its Access Point interface ap0
+sudo ifconfig ap0 10.0.0.1 netmask 255.255.255.0 up
+
+# assign bridge an IP address and bring it up
+sudo ifconfig br0 192.168.3.1 netmask 255.255.255.0 up
+
+# start the hostapd and dnsmasq services
+sudo service hostapd start
+sudo service dnsmasq start
+
+# set the hostapd and dnsmasq services to autostart
+sudo update-rc.d hostapd enable
+sudo update-rc.d dnsmasq enable
