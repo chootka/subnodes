96cbeb3250c7efa0788fb379e832d74facbf517f
Sarah Grant <sgrant@barbariangroup.com>
2013-06-09 02:15:12 -0400

    restore mesh script

--- scripts/subnodes.sh
+++ scripts/subnodes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # /etc/init.d/subnodes
-# starts up wlan0 interface ( and at some point, batman-adv )
-# starts up hostapd => broadcasting wireless network HOT PROBS
+# starts up ap0 and mesh0, bat0 interfaces
+# starts up hostapd => broadcasting wireless network subnodes
 # starts up node app
 
 #TODO move app to /usr/bin/
@@ -17,8 +17,45 @@ SCRIPTNAME=/etc/init.d/$NAME
 
 	case "$1" in
 		start)
-			echo "Starting $NAME and hostapd!"
-			sudo hostapd -B /etc/hostapd/hostapd.conf
+			echo "Starting $NAME access point and mesh point..."
+			# delete default interfaces
+			$DAEMON ifconfig wlan0 down
+			$DAEMON iw dev wlan0 del
+			$DAEMON ifconfig wlan1 down
+			$DAEMON iw dev wlan1 del
+
+			# create the ap0 and mesh0 interfaces
+			$DAEMON iw phy phy0 interface add ap0 type __ap
+			$DAEMON iw phy phy1 interface add mesh0 type adhoc
+			$DAEMON ifconfig mesh0 mtu 1528
+			$DAEMON iwconfig mesh0 mode ad-hoc essid meshnet ap 02:12:34:56:78:90 channel 3
+			$DAEMON ifconfig mesh0 down
+
+			# add the interface to batman
+			$DAEMON batctl if add mesh0
+			$DAEMON batctl ap_isolation 1
+
+			# add interfaces to the bridge
+			$DAEMON brctl addbr br0
+			$DAEMON brctl addif br0 ap0
+			$DAEMON brctl addif br0 bat0
+
+			# bring up the BATMAN adv interface
+			$DAEMON ifconfig mesh0 up
+			$DAEMON ifconfig bat0 up
+
+			# bring up the AP interface and give ap0 a static IP
+			# not sure if ap0 needs an IP anymore, since it is part of the bridge
+			$DAEMON ifconfig ap0 10.0.0.1 netmask 255.255.255.0 up
+
+			# bring up the brdige and assign it a static IP
+			$DAEMON ifconfig br0 192.168.3.1 netmask 255.255.255.0 up
+
+			# start the hostapd and dnsmasq services
+			$DAEMON service hostapd start
+			$DAEMON service dnsmasq start
+
+			# start the node.js chat application
 			cd $DAEMON_PATH
 			PID=`$DAEMON $DAEMONOPTS > /dev/null 2>&1 & echo $!`
 			#echo "Saving PID" $PID " to " $PIDFILE
