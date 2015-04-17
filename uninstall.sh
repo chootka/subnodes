36ea60d81d32a26b7738776a13c34e31b304834a
chootka <com@chootka.com>
2015-04-17 09:10:54 -0400

    missing semicolons in uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -17,7 +17,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Uninstall Subnodes
 #
-read -p "Do you wish to uninstall subnodes from your Raspberry Pi? Hitting return will continue with the default 'No' option and exit this script." yn
+read -p "Do you wish to uninstall subnodes from your Raspberry Pi? Hitting return will continue with the default 'No' option and exit this script. " yn
 case $yn in
 	[Yy]* )
 		clear
@@ -65,11 +65,12 @@ case $yn in
 		cd /home/pi/
 		rm -rf /home/pi/subnodes
 		echo -en "[OK]\n"
-		read -p "Do you wish to reboot now? Hitting return will continue with the default 'No' option and exit this script." yn
+		read -p "Do you wish to reboot now? Hitting return will continue with the default 'No' option and exit this script. " yn
 		case $yn in
 			[Yy]* )
-				reboot
+				reboot;;
 			[Nn]* ) exit 0;;
+		esac
 
 	;;
 	[Nn]* ) exit 0;;
